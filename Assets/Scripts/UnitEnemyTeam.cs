5fbc911cb4fff3974e3632741623b89539b57058
ADavlatov <enderbro306@gmail.com>
2022-03-30 20:19:35 +0300

    Переименовал для удобства

--- Assets/Scripts/UnitEnemyTeam.cs
+++ Assets/Scripts/UnitEnemyTeam.cs
@@ -2,12 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class UnitRedTeam : MonoBehaviour
+public class UnitEnemyTeam : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
         transform.GetComponent<MeshRenderer>().material.color = Color.red;
-        SelectObjects.unitRedTeam.Add(gameObject);
+        UnitSelect.unit.Add(gameObject);
     }
 }
--- Assets/Scripts/UnitOwnTeam.cs
+++ Assets/Scripts/UnitOwnTeam.cs
@@ -2,13 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class UnitBlueTeam : MonoBehaviour
+public class UnitOwnTeam : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
         transform.GetComponent<MeshRenderer>().material.color = Color.blue;
-        SelectObjects.unitBlueTeam.Add(gameObject);
+        UnitSelect.unit.Add(gameObject);
     }
-    
 }
